b4e4e083df8fb90e50714d499ec1befad88f875a
nut <nuttadetpn45@gmail.com>
2017-08-31 19:01:34 +0700

    no message

--- Hello.java
+++ Hello.java
@@ -7,7 +7,7 @@ class Hello {
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
-        System.out.println("Trainee - Krerk Piromsopa, Ph.D. --- ");
+        System.out.println("Trainee - NUTTADET ORASA, N.O --- ");
     }
     
     public static void main(String [] argv) {
