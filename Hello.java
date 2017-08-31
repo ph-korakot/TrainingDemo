1388da78ac7c6445809248c2c7dc62c91f2a87c8
Kittikun <kittikun.sienarm@gmail.com>
2017-08-31 19:24:53 +0700

    Create Method Kittikun  In Hello.java

--- Hello.java
+++ Hello.java
@@ -3,11 +3,16 @@ class Hello {
     public Hello() {
         System.out.println("Hello! We are:");
         this.Krerk();
+		this.Kittikun();
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
         System.out.println("Trainee - Krerk Piromsopa, Ph.D. --- ");
+    }
+	
+    private void Kittikun() {
+        System.out.println("Git exame Kittikun Sienarm");
     }
     
     public static void main(String [] argv) {
