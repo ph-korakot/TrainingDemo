760d9ea71ab3810f5a2ee69f9c4d895412d2437f
somchai.c <somchaic@motiftech.com>
2017-08-31 19:51:29 +0700

    add print Beaphin

--- Hello.java
+++ Hello.java
@@ -3,10 +3,15 @@ class Hello {
     public Hello() {
         System.out.println("Hello! We are:");
         this.Krerk();
+		this.Beaphin();
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
+    }
+	
+	private void Beaphin() {
+        System.out.println("Beaphin Somchai");
     }
     
     public static void main(String [] argv) {
