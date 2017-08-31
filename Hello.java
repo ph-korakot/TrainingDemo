6c88a85ccdc8711902c95602970c5078fd3180c2
MOTIF\varunee.k <varunee9392@gmail.com>
2017-08-31 19:34:39 +0700

    no message

--- Hello.java
+++ Hello.java
@@ -3,10 +3,15 @@ class Hello {
     public Hello() {
         System.out.println("Hello! We are:");
         this.Krerk();
+		this.Varunee();
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
+    }
+	
+	private void Varunee () {
+        System.out.println("Trainer - Varunee Komonsubin, SA");
     }
     
     public static void main(String [] argv) {
