e4cd7beee10bec2ea68742cc2aa0ceaa6886fe8b
Tanachai <tanachai.r@motiftech.com>
2017-08-31 19:33:32 +0700

    add gitignore and create method name Tanachai

--- Hello.java
+++ Hello.java
@@ -3,12 +3,18 @@ class Hello {
     public Hello() {
         System.out.println("Hello! We are:");
         this.Krerk();
+        this.Tanachai();
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
         System.out.println("Trainee - Krerk Piromsopa, Ph.D. --- ");
     }
+    
+    private void Tanachai() {
+      System.out.println("Tanachai Rugvong");
+      System.out.println("Tanachai Rugvong");
+  }
     
     public static void main(String [] argv) {
         new Hello();
