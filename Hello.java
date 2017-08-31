77381ad32af90040279b593f88dd926670bfe725
nut <nuttadetpn45@gmail.com>
2017-08-31 19:13:03 +0700

    no message

--- Hello.java
+++ Hello.java
@@ -3,11 +3,15 @@ class Hello {
     public Hello() {
         System.out.println("Hello! We are:");
         this.Krerk();
+		this.Nuttadet();
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
-        System.out.println("Trainee - NUTTADET ORASA, N.O --- ");
+    }
+	
+    private void Nuttadet() {
+        System.out.println("Trainee - Nuttadet Orasa, Ph.D.");
     }
     
     public static void main(String [] argv) {
