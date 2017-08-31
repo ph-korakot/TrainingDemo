5868c2c4b6d88a453a17ec70a4733af588a299e7
Sasimaporn Sillapasit <zsasimaporn.s@motif.corp>
2017-08-31 19:43:43 +0700

    ice

--- Hello.java
+++ Hello.java
@@ -3,10 +3,15 @@ class Hello {
     public Hello() {
         System.out.println("Hello! We are:");
         this.Krerk();
+        this.Sasimaporn();
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
+    }
+   
+    private void Sasimaporn() {
+        System.out.println("Trainer - Sasimaporn Sillapasit, Ph.D.");
     }
     
     public static void main(String [] argv) {
