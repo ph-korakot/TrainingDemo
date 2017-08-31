175bd903e4e25a9ad57af8cb4fe5240b057345c5
Juthamas Kroswas <jkroswas@gmail.com>
2017-08-31 19:31:26 +0700

    create .gitignore and edit java file

--- Hello.java
+++ Hello.java
@@ -3,14 +3,23 @@ class Hello {
     public Hello() {
         System.out.println("Hello! We are:");
         this.Krerk();
+		this.Juthamas();
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
         System.out.println("Trainee - Krerk Piromsopa, Ph.D. --- ");
     }
+	
+	private void Juthamas() {
+        System.out.println("Juthamas Kroswas");
+        System.out.println("Juthamas Kroswas");
+    }
     
     public static void main(String [] argv) {
         new Hello();
+		
     }
+	
+	
 }
