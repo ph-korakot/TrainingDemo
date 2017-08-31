7931374511747a7ac3935e0bfb4f44109057f832
Surewat.i <surewat.i@motiftech.com>
2017-08-31 19:08:13 +0700

    fix method name

--- Hello.java
+++ Hello.java
@@ -2,13 +2,15 @@ class Hello {
     
     public Hello() {
         System.out.println("Hello! We are:");
-        this.Krerk();
+        this.BEST();
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
     }
-    
+    private void BEST() {
+        System.out.println("Trainer - Surewat Intharasuwan");
+    }
     public static void main(String [] argv) {
         new Hello();
     }
