67d782cf16ae07b653a27870482282cb545fc37a
MOTIF\pattanan.b <pattanan.boonsiri@gmail.com>
2017-08-31 20:27:32 +0700

    fang

--- Hello.java
+++ Hello.java
@@ -3,12 +3,17 @@ class Hello {
     public Hello() {
         System.out.println("Hello! We are:");
         this.Krerk();
+		this.pattanan();
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
     }
-    
+	
+    private void pattanan() {
+        System.out.println("Trainer - pattanan Boonsiri");
+    }
+	
     public static void main(String [] argv) {
         new Hello();
     }
