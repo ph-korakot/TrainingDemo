4befcac5be37836ce9f8f31a93e9d8e59301bb0e
korakot.p <korakot.p@motiftech.com>
2017-09-07 16:46:32 +0700

    korakot commit

--- Hello.java
+++ Hello.java
@@ -3,11 +3,17 @@ class Hello {
     public Hello() {
         System.out.println("Hello! We are:");
         this.Krerk();
+		this.korakot();
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
         System.out.println("Trainee - Krerk Piromsopa, Ph.D. --- ");
+    }
+	
+	 private void korakot() {
+        System.out.println("Trainer - korakot phokhaw, Ph.D.");
+        System.out.println("Trainee - korakot phokhaw, Ph.D. --- ");
     }
     
     public static void main(String [] argv) {
